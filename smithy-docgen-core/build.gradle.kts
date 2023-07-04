1e02f7f5d3a30c9bf84336f6e072472a3d4fa650
Jose Vitor Schneid <jvsilva@amazon.com>
2023-07-04 11:09:28 -0300

    Plugin repository scaffolding

---
+++ smithy-docgen-core/build.gradle.kts
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+description = "This module contains support for generating API documentation " +
+        "based on Smithy models."
+extra["displayName"] = "Smithy :: DocGen :: Core"
+extra["moduleName"] = "software.amazon.smithy.docgen.core"
+
+val smithyVersion: String by project
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+}
+
+dependencies {
+    api("software.amazon.smithy:smithy-build:$smithyVersion")
+}
