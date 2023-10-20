f0189cb5706e567e45356978182569bfb94f4e73
Jordon Phillips <JordonPhillips@users.noreply.github.com>
2023-10-20 15:52:36 +0200

    Automatically build sphinx projects
    This adds automatic html builds for sphinx projects by creating a
    python virtual environment, installing sphinx into it, and then
    using that sphinx to do the build. This has the advantage of ensuring
    the build is completely isolated from the system python, so we can
    ensure the precise versions we want without issue.
    
    The disadvantage of doing it this way is that it's slow since it has
    to create a virtualenv on every invocation and for every projection
    since projections run in parallel and a shared cache can't be used.
    One potential alternative is to just check to see is sphinx and the
    required dependencies are already installed, but that requires
    parsing the python dependency format. If we had projection config,
    we could also just make sphinx-build binary an option.

---
+++ smithy-docgen-core/src/main/java/software/amazon/smithy/docgen/core/DocgenUtils.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package software.amazon.smithy.docgen.core;
+
+import static java.lang.String.format;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import software.amazon.smithy.codegen.core.CodegenException;
+import software.amazon.smithy.utils.SmithyUnstableApi;
+
+/**
+ * Provides various utility methods.
+ */
+@SmithyUnstableApi
+public final class DocgenUtils {
+
+    private static final Logger LOGGER = Logger.getLogger(DocgenUtils.class.getName());
+
+    private DocgenUtils() {}
+
+    /**
+     * Executes a given shell command in a given directory.
+     *
+     * @param command The string command to execute, e.g. "sphinx-build".
+     * @param directory The directory to run the command in.
+     * @return Returns the console output of the command.
+     */
+    public static String runCommand(String command, Path directory) {
+        String[] finalizedCommand;
+        if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+            finalizedCommand = new String[]{"cmd.exe", "/c", command};
+        } else {
+            finalizedCommand = new String[]{"sh", "-c", command};
+        }
+
+        ProcessBuilder processBuilder = new ProcessBuilder(finalizedCommand)
+            .redirectErrorStream(true)
+            .directory(directory.toFile());
+
+        try {
+            Process process = processBuilder.start();
+            List<String> output = new ArrayList<>();
+
+            // Capture output for reporting.
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+                process.getInputStream(), Charset.defaultCharset()))) {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    LOGGER.finest(line);
+                    output.add(line);
+                }
+            }
+
+            process.waitFor();
+            process.destroy();
+
+            String joinedOutput = String.join(System.lineSeparator(), output);
+            if (process.exitValue() != 0) {
+                throw new CodegenException(format(
+                    "Command `%s` failed with output:%n%n%s", command, joinedOutput));
+            }
+            return joinedOutput;
+        } catch (InterruptedException | IOException e) {
+            throw new CodegenException(e);
+        }
+    }
+}
