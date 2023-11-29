6be6b21a34776adf7bcd143e128f797d63ea6f42
Jordon Phillips <JordonPhillips@users.noreply.github.com>
2023-11-29 15:15:03 +0100

    Add base httpQuery trait support

--- smithy-docgen-core/src/main/java/software/amazon/smithy/docgen/core/integrations/BuiltinsIntegration.java
+++ smithy-docgen-core/src/main/java/software/amazon/smithy/docgen/core/integrations/BuiltinsIntegration.java
@@ -16,6 +16,7 @@ import software.amazon.smithy.docgen.core.interceptors.DeprecatedInterceptor;
 import software.amazon.smithy.docgen.core.interceptors.ErrorFaultInterceptor;
 import software.amazon.smithy.docgen.core.interceptors.ExternalDocsInterceptor;
 import software.amazon.smithy.docgen.core.interceptors.HttpInterceptor;
+import software.amazon.smithy.docgen.core.interceptors.HttpQueryInterceptor;
 import software.amazon.smithy.docgen.core.interceptors.IdempotencyInterceptor;
 import software.amazon.smithy.docgen.core.interceptors.InternalInterceptor;
 import software.amazon.smithy.docgen.core.interceptors.JsonNameInterceptor;
@@ -88,6 +89,7 @@ public class BuiltinsIntegration implements DocIntegration {
                 new XmlAttributeInterceptor(),
                 new XmlNameInterceptor(),
                 new XmlFlattenedInterceptor(),
+                new HttpQueryInterceptor(),
                 new HttpInterceptor(),
                 new PaginationInterceptor(),
                 new RequestCompressionInterceptor(),
