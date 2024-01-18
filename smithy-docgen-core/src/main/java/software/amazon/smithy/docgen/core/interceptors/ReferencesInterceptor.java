3f5033752eba45626539c13a9ed67d591905eac5
Jordon Phillips <JordonPhillips@users.noreply.github.com>
2024-01-18 12:30:47 +0100

    Only reference resources contained in the service
    This updates the resource docs to check whether a resource is bound
    to the service rather than whether it's just in the model to
    determine whether it is an external resource or not.

--- smithy-docgen-core/src/main/java/software/amazon/smithy/docgen/core/interceptors/ReferencesInterceptor.java
+++ smithy-docgen-core/src/main/java/software/amazon/smithy/docgen/core/interceptors/ReferencesInterceptor.java
@@ -9,10 +9,12 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import software.amazon.smithy.docgen.core.DocGenerationContext;
 import software.amazon.smithy.docgen.core.sections.ShapeDetailsSection;
 import software.amazon.smithy.docgen.core.writers.DocWriter;
 import software.amazon.smithy.docgen.core.writers.DocWriter.NoticeType;
+import software.amazon.smithy.model.knowledge.TopDownIndex;
 import software.amazon.smithy.model.shapes.Shape;
 import software.amazon.smithy.model.traits.ReferencesTrait;
 import software.amazon.smithy.model.traits.ReferencesTrait.Reference;
@@ -56,12 +58,16 @@ public final class ReferencesInterceptor implements CodeInterceptor.Appender<Sha
         var symbolProvider = section.context().symbolProvider();
         var localRefs = getLocalReferences(section.context(), section.shape());
         var externalRefs = section.context().settings().references();
+        var serviceResources = TopDownIndex.of(model).getContainedResources(section.context().settings().service())
+                .stream()
+                .map(Shape::getId)
+                .collect(Collectors.toSet());
 
         // This is a mapping of reference link to optional rel type. If `rel` isn't set,
         // it'll be an empty optional that won't get displayed.
         var references = new LinkedHashMap<>(localRefs.size());
         for (var reference : localRefs) {
-            if (model.getShape(reference.getResource()).isPresent()) {
+            if (serviceResources.contains(reference.getResource())) {
                 var symbol = symbolProvider.toSymbol(model.expectShape(reference.getResource()));
                 references.put(symbol, reference.getRel());
             } else if (externalRefs.containsKey(reference.getResource())) {
@@ -99,11 +105,17 @@ public final class ReferencesInterceptor implements CodeInterceptor.Appender<Sha
         for (var member : shape.members()) {
             references.addAll(getLocalReferences(context, member));
         }
+
         var shapeRefs = shape.getMemberTrait(model, ReferencesTrait.class);
         var externalsRefs = context.settings().references();
+        var serviceResources = TopDownIndex.of(model).getContainedResources(context.settings().service())
+                .stream()
+                .map(Shape::getId)
+                .collect(Collectors.toSet());
+
         if (shapeRefs.isPresent()) {
             for (var reference : shapeRefs.get().getReferences()) {
-                if (model.getShape(reference.getResource()).isPresent()
+                if (serviceResources.contains(reference.getResource())
                         || externalsRefs.containsKey(reference.getResource())) {
                     references.add(reference);
                 } else {
